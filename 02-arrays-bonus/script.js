ecac96894d0f5e748dfa7feb391d259c5113405f
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-08 17:42:56 +0200

    documeto 2 esercizio 4 fatto

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -41,7 +41,14 @@ console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+let isFabioPresent = false;
+for (let i = 0; i < teachers.length; i++) {
+  const teacher = teachers[i]; 
+  if (teacher === 'Fabio') {   
+    isFabioPresent = true;     
+  }
+}
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
